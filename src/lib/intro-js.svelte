ee5e8089e90328eb609cc8a3c36e784c45832ccc
Emrullah TUNCAY <etuncay.com@gmail.com>
2023-11-15 08:00:31 +0300

    initial

---
+++ src/lib/intro-js.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import IntroJs from 'intro.js'
+    import 'intro.js/minified/introjs.min.css'
+    import {onMount} from 'svelte'
+    
+    export let options: any= {}
+    onMount(() => {
+        if(options!=null && options!.steps!=null && options!.steps!.length>0){
+            for(let i=0; i<options!.steps!.length;i++){
+                let step = options!.steps[i];
+                if(step!.selector!=null){
+                    options!.steps[i].element= document.querySelector(step!.selector);
+                }
+            } 
+        }
+        IntroJs().setOptions(options).start();
+    });
+</script>
+
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script >
+	import IntroJs from "$lib/intro-js.svelte";
+
+      
+    let options = {
+          steps: [{
+              title: 'Welcome',
+              intro: 'Hello World! 👋json'
+          },
+          {
+              selector: '.card-demo',
+              intro: 'This step focuses on an image json'
+          },
+          {
+              title: 'Farewell!',           
+              selector: '.card__image',
+              intro: 'And this is our final step! json'
+          }]
+        };
+  </script>
+  <IntroJs {options}>
+    <div data-title="Welcome!" data-intro="Hello World! 👋" class="card-demo">
+        <div class="card shadow--md">
+          <div class="card__image" data-intro="Intro.js can highlight on elements">
+            <img src="https://fakeimg.pl/300/" alt="Image alt text" title="Logo Title Text 1" />
+          </div>
+          <div class="card__body" data-title="Farewell!" data-intro="And this is the last step!">
+            <h4>Quaco Lighthouse</h4>
+            <small>
+              The Quaco Head Lighthouse is a well maintained lighthouse close to St.
+              Martins. It is a short, beautiful walk to the lighthouse along the
+              seashore.
+            </small>
+          </div>
+        </div>
+      </div>
+  </IntroJs>
